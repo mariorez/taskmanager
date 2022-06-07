03bea8f005321bfeba627353354f75a3fad8e867
Mario Rezende <mariorez@gmail.com>
2022-06-07 17:51:52 -0300

    clean up

--- src/main/kotlin/org/seariver/taskmanager/TaskmanagerApplication.kt
+++ src/main/kotlin/org/seariver/taskmanager/TaskmanagerApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-open class TaskmanagerApplication
+class TaskmanagerApplication
 
 fun main(args: Array<String>) {
     runApplication<TaskmanagerApplication>(*args)
